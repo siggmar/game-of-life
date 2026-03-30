e97ce2e9bb0f11bc631ecf88f5745012bc32c1ff
siggmar <subberman0822@gmail.com>
2026-03-30 22:42:43 +0200

    basic Conways game of life

---
+++ Makefile
@@ -0,0 +1,9 @@
+
+main: main.c
+	gcc main.c -o main
+
+run: main
+	./main
+
+clean:
+	rm main
---
+++ main.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#define ROWS 44
+#define COLS 132
+
+typedef enum { DEAD = 0, ALIVE } States;
+
+int front_buf[ROWS][COLS] = {0}; // last
+int back_buf[ROWS][COLS] = {0};  // next
+
+int step_c = 0;
+
+void display(void);
+void step(void);
+int get_neboures(int center_y, int center_x);
+void randomize_grid(void);
+
+int main(void)
+{
+    // Glider:
+    // front_buf[0][1] = 1;
+    // front_buf[1][2] = 1;
+    // front_buf[2][0] = 1;
+    // front_buf[2][1] = 1;
+    // front_buf[2][2] = 1;
+
+    // sidways L
+    // front_buf[0][0] = 1;
+    // front_buf[0][1] = 1;
+    // front_buf[0][2] = 1;
+    // front_buf[1][0] = 1;
+
+    // Line
+    // front_buf[2][3] = 1;
+    // front_buf[3][3] = 1;
+    // front_buf[4][3] = 1;
+
+    randomize_grid();
+
+    for (;;) {
+        display();
+        step();
+        memcpy(front_buf, back_buf, sizeof(front_buf));
+        printf("\033[%dA\033[%dD", ROWS, COLS);
+        usleep(100 * 1000);
+    }
+
+    return 0;
+}
+
+void display(void)
+{
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            if (front_buf[y][x] == ALIVE)
+                printf("#");
+            else
+                printf(".");
+        }
+        printf("\n");
+    }
+}
+
+void step(void)
+{
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            int neboures = get_neboures(y, x);
+
+            if (front_buf[y][x] == ALIVE) {
+                back_buf[y][x] = (neboures == 2 || neboures == 3);
+            } else {
+                back_buf[y][x] = (neboures == 3);
+            }
+        }
+    }
+}
+int mod(int a, int b) { return (a % b + b) % b; }
+
+int get_neboures(int center_y, int center_x)
+{
+    // -1, -1 | -1, 0  | -1, 1
+    // 0, -1  |  0, 0  | 0, 1
+    // 1, -1  |  1, 0  | 1, 1
+
+    int neboures = 0;
+
+    for (int dy = -1; dy <= 1; dy++) {
+        for (int dx = -1; dx <= 1; dx++) {
+            if (!(dy == 0 && dx == 0)) {
+                int x = mod(center_x + dx, COLS);
+                int y = mod(center_y + dy, ROWS);
+
+                if (front_buf[y][x] == ALIVE) neboures += 1;
+            }
+        }
+    }
+    return neboures;
+}
+
+void randomize_grid(void)
+{
+    srand(time(NULL));
+
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            if (rand() % 100 >= 50) {
+                front_buf[y][x] = ALIVE;
+            }
+        }
+    }
+}
